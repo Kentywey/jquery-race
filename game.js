785dfb7c81c1f4286f25a60de2c2b554e7e1956e
Weymeesch.kenty <kweymeesch@gmail.com>
2016-03-21 16:48:04 +0100

    Initial commit

---
+++ game.js
@@ -0,0 +1,14 @@
+$(document).ready(function){
+	var winner = null;
+	var player_1 = $()
+	var player_2 = $()
+
+  function MoveForward(element){
+   $().toggleClass('active');
+ }
+
+ $(document).on('keyup', function(event)){
+
+ });
+
+});
